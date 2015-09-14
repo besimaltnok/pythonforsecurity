918b8733bfecbc9069f8ae0c4aa60a6092cec3f8
BesimAltnok <besimaltnok@gmail.com>
2015-09-15 02:04:16 +0300

    Create findOpenWifiNet_scapy.py

---
+++ findOpenWifiNet_scapy.py
@@ -0,0 +1,25 @@
+__author__ = 'besimaltnok'
+
+from scapy.all import *
+
+open_networks = []
+ap_list       = []
+
+
+def PacketHandlers(pkt) :
+
+	if pkt.haslayer(Dot11) :
+		if pkt.type == 0 and pkt.subtype == 8 :
+			if pkt.addr2 not in ap_list :
+				ap_list.append(pkt.addr2)
+				open_ssid  = pkt.info
+				open_bssid = pkt.addr2
+				cap = pkt.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}"
+									 "{Dot11ProbeResp:%Dot11ProbeResp.cap%}").split('+')
+
+				if 'privacy' not in cap and pkt.info != '':
+					print "Detect Open Network MAC Addres : %s and SSID : %s " %(open_bssid, open_ssid)  
+
+
+
+sniff(iface='wlan0mon', count=999999, prn=PacketHandlers)

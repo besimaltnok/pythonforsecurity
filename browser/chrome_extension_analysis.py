e7a37f1a579a5464c51b06ed33e83273e3881e4c
BesimAltnok <besimaltnok@gmail.com>
2015-10-13 19:46:33 +0300

    Update chrome_extension_analysis.py

--- browser/chrome_extension_analysis.py
+++ browser/chrome_extension_analysis.py
@@ -3,3 +3,11 @@ __author__ = "besimaltnok"
 import json
 import os
 
+
+version  = ""
+name     = ""
+manifest = extensions_save + version + name 
+extensions_save = "C:\Users\arch\AppData\Local\Google\Chrome\User Data\Default\Extensions"
+ext_list = os.listdir(extensions_save)
+
+

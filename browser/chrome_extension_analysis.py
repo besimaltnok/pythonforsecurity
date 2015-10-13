5ea16eaff795147858571127c3d206aeed612ef4
BesimAltnok <besimaltnok@gmail.com>
2015-10-13 21:49:32 +0300

    Update chrome_extension_analysis.py

--- browser/chrome_extension_analysis.py
+++ browser/chrome_extension_analysis.py
@@ -4,10 +4,20 @@ import json
 import os
 
 
-version  = ""
-name     = ""
-manifest = extensions_save + version + name 
-extensions_save = "C:\Users\arch\AppData\Local\Google\Chrome\User Data\Default\Extensions"
-ext_list = os.listdir(extensions_save)
+extensions_path = "C:\Users\\arch\AppData\Local\Google\Chrome\User Data\Default\Extensions\\"
+ext_list =  os.listdir(extensions_path)
+for j in range(len(ext_list)):
+    plugin_id = ext_list[j]
+    manifest = os.listdir(extensions_path+plugin_id)
+    if len(manifest) == 0:
+        pass
+    else:
+        full_path = extensions_path+ext_list[j] + "\\" + manifest[0] + "\\" + "manifest.json"
+        with open(full_path) as data_file:
+            data = json.load(data_file)
+            print "Name        : %s\n" %(data['name'])
+            print "Plugin id   : %s\n" %(plugin_id)
+            print "Permission  : %s\n" %(data['permissions'])
+            print "******************************************"
 
 

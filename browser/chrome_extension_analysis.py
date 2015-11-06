c31adb6abc784f4c4c8e78ea717165c7241b4929
BesimAltnok <besimaltnok@gmail.com>
2015-11-06 19:25:07 +0200

    Update chrome_extension_analysis.py

--- browser/chrome_extension_analysis.py
+++ browser/chrome_extension_analysis.py
@@ -6,16 +6,15 @@ import os
 
 extensions_path = "C:\Users\\arch\AppData\Local\Google\Chrome\User Data\Default\Extensions\\"
 ext_list =  os.listdir(extensions_path)
+print(ext_list)
 for j in range(len(ext_list)):
     plugin_id = ext_list[j]
-    manifest = os.listdir(extensions_path+plugin_id)
-    if manifest is not None:
-        full_path = extensions_path+ext_list[j] + "\\" + manifest[0] + "\\" + "manifest.json"
+    version = os.listdir(extensions_path+plugin_id)
+    if version is not None:
+        full_path = extensions_path+ext_list[j] + "\\" + version[0] + "\\" + "manifest.json"
         with open(full_path) as data_file:
             data = json.load(data_file)
-            print "Name        : %s\n" %(data['name'])
-            print "Plugin id   : %s\n" %(plugin_id)
-            print "Permission  : %s\n" %(data['permissions'])
-            print "******************************************"
-
-
+            print("Name        : {}\n").format(data['name'])
+            print("Plugin id   : {}\n").format(plugin_id)
+            print("Permission  : {}\n").format(data['permissions'])
+            print("*" * 40)

348efe78b466d7aab6425578af7c417ee9a05658
BesimAltnok <besimaltnok@gmail.com>
2016-02-19 23:30:26 +0200

    Update findOpenWifiNet_scapy.py

--- wifisniff/findOpenWifiNet_scapy.py
+++ wifisniff/findOpenWifiNet_scapy.py
@@ -6,20 +6,19 @@ open_networks = []
 ap_list       = []
 
 
-def PacketHandlers(pkt) :
+def FindOpenNet(pkt) :
+	
+	if pkt.type == 0 and pkt.subtype == 8 :
+		if pkt.addr2 not in ap_list :
+			ap_list.append(pkt.addr2)
+			open_ssid  = pkt.info
+			open_bssid = pkt.addr2
+			cap = pkt.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}"
+								 "{Dot11ProbeResp:%Dot11ProbeResp.cap%}").split('+')
 
-	if pkt.haslayer(Dot11) :
-		if pkt.type == 0 and pkt.subtype == 8 :
-			if pkt.addr2 not in ap_list :
-				ap_list.append(pkt.addr2)
-				open_ssid  = pkt.info
-				open_bssid = pkt.addr2
-				cap = pkt.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}"
-									 "{Dot11ProbeResp:%Dot11ProbeResp.cap%}").split('+')
-
-				if 'privacy' not in cap and pkt.info != '':
-					print "Detect Open Network MAC Addres : %s and SSID : %s " %(open_bssid, open_ssid)  
-
+			if 'privacy' not in cap and pkt.info != '':
+				print "Detect Open Network MAC Addres : %s and SSID : %s " %(open_bssid, open_ssid)  
 
 
-sniff(iface='wlan0mon', count=999999, prn=PacketHandlers)
+
+sniff(iface='wlan0', prn=FindOpenNet)
